6fb13986729b46bc6e82045f40a7aa88170e0c85
Felipe Moreira Fernandes <felipe0731@digitalcollegesul.local>
2024-05-18 16:55:32 -0300

    home e header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,22 @@
+import styled from "styled-components";
+import { IconField } from 'primereact/iconfield';
+import { InputIcon } from 'primereact/inputicon';
+import { InputText } from "primereact/inputtext";
+
+const HeaderContainer = styled.header``
+
 const Header = () => {
-    return ( 
-    <>
-        header
-    </>
+    return (
+        <>
+            <HeaderContainer className="w-12 flex align-items-center gap-4 px-8 md:px-8 bg-primary">
+                <h1>LOGO</h1>
+                <IconField iconPosition="right" className="flex-1 md:flex-none md:w-4">
+                    <InputIcon className="pi pi-search cursor-pointer" />
+                    <InputText type="text" placeholder="Buscar tarefa..." className="w-full" />
+                </IconField>
+            </HeaderContainer>
+        </>
     );
 }
- 
+
 export default Header;
--- src/pages/sitePages/Home/index.jsx
+++ src/pages/sitePages/Home/index.jsx
@@ -1,9 +1,64 @@
+import { Button } from "primereact/button";
+import styled from "styled-components";
+import { Dialog } from 'primereact/dialog';
+import { InputTextarea } from "primereact/inputtextarea";
+import { InputText } from "primereact/inputtext";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+
+const HomeContainer = styled.section``
+
 const Home = () => {
-    return ( 
+
+    const [visibleDialog, setVisibleDialog] = useState(false);
+    const { register, handleSubmit } = useForm();
+
+    const criarTarefa = (dados) => {
+        console.log(dados)
+    }
+
+    return (
         <>
-            Home
+            <HomeContainer className="px-8 py-6">
+                <h1 className="flex justify-content-between align-items-center">
+                    tarefas
+                    <Button
+                        icon="pi pi-plus"
+                        label="Nova Tarefa"
+                        onClick={(e) => setVisibleDialog(true)}
+                    />
+                </h1>
+
+                <Dialog
+                    visible={visibleDialog}
+                    header="Nova tarefa"
+                    onHide={(e) => setVisibleDialog(false)}
+                >
+                    <form
+                        onSubmit={handleSubmit(criarTarefa)}
+                        className="flex flex-column gap-3"
+                    >
+                        <InputText
+                            placeholder="Nome da tarefa"
+                            {...register("nome", { required: true })}
+                        />
+                        <InputTextarea
+                            placeholder="Descrição da tarefa"
+                            className="h-8rem"
+                            autoResize
+                            {...register("descricao", { required: true })}
+                        />
+                        <Button
+                            type="submit"
+                            label="Salvar"
+                        />
+
+                    </form>
+                </Dialog>
+            </HomeContainer>
         </>
-     );
+    );
 }
- 
+
 export default Home;
